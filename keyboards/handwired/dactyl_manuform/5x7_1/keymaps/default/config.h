7e517c91c8a2adebedc966ee45572f7dbc43183f
Dustin Schultz <dustindschultz@gmail.com>
2024-06-20 11:37:40 -0400

    got left side with atmega flashing

--- keyboards/handwired/dactyl_manuform/5x7_1/keymaps/default/config.h
+++ keyboards/handwired/dactyl_manuform/5x7_1/keymaps/default/config.h
@@ -18,19 +18,30 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #pragma once
 
 #define MASTER_RIGHT
+// #define MASTER_LEFT
 // #define DEBOUNCE 5
 
-// #define SERIAL_USART_FULL_DUPLEX   // Enable full duplex operation mode
+#define SERIAL_USART_FULL_DUPLEX   // Enable full duplex operation mode
+#define SERIAL_USART_RX_PIN D2
+#define SERIAL_USART_TX_PIN D3
 // // #define SERIAL_USART_HALF_DUPLEX   // Enable half duplex operation mode
 // #define SERIAL_USART_RX_PIN GP0
 // #define SERIAL_USART_TX_PIN GP1
 // #define SERIAL_USART_TX_PIN GP0
 
-#define SPLIT_HAND_PIN GP26 // Shunt from 3v to GP26 on left side
+// #define SPLIT_HAND_PIN F4 // No shunt here, settings different for other hand
+// #define SPLIT_HAND_PIN_LOW_IS_LEFT True
+
+#define USE_SERIAL
+
+#define SOFT_SERIAL_PIN D0
 
 // #define SPLIT_USB_DETECT
+#define SPLIT_HAND_MATRIX_GRID D1, F4
+#define	SPLIT_HAND_PIN_LOW_IS_LEFT
 // #define SOFT_SERIAL_PIN GP0
 
 
 // #define ENCODERS_PAD_A { GP27 }
 // #define ENCODERS_PAD_B { GP28 }
+
