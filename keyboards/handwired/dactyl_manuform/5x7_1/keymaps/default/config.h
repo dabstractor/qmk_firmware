4a1292d22664c09d31b834c03f3bb8c62c045fac
Dustin Schultz <dustindschultz@gmail.com>
2024-06-30 08:58:58 -0400

    got both sides working with pro micro controllers

--- keyboards/handwired/dactyl_manuform/5x7_1/keymaps/default/config.h
+++ keyboards/handwired/dactyl_manuform/5x7_1/keymaps/default/config.h
@@ -21,7 +21,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // #define MASTER_LEFT
 // #define DEBOUNCE 5
 
-#define SERIAL_USART_FULL_DUPLEX   // Enable full duplex operation mode
+// #define SERIAL_USART_FULL_DUPLEX   // Enable full duplex operation mode
+// #define SERIAL_USART_RX_PIN D2
+// #define SERIAL_USART_TX_PIN D3
 // // #define SERIAL_USART_HALF_DUPLEX   // Enable half duplex operation mode
 #define SERIAL_USART_RX_PIN GP0
 #define SERIAL_USART_TX_PIN GP1
@@ -32,14 +34,27 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define USE_SERIAL
 
-#define SOFT_SERIAL_PIN D0
+#define SOFT_SERIAL_PIN D2
 
-// #define SPLIT_USB_DETECT
-#define SPLIT_HAND_MATRIX_GRID D1, F4
-#define	SPLIT_HAND_PIN_LOW_IS_LEFT
+#define SPLIT_USB_DETECT
+
+// #define ENCODERS_PAD_A { F4 }
+// #define ENCODERS_PAD_B { D1 }
+// #define ENCODER_RESOLUTION 4
+
+// Define if your encoder has a button
+#define ENCODER_BUTTON_PIN D0
+
+// #define SPLIT_HAND_MATRIX_GRID D1, F4
+// #define SPLIT_HAND_PIN F4
+// #define	SPLIT_HAND_PIN_LOW_IS_LEFT
 // #define SOFT_SERIAL_PIN GP0
 
+// #define SPLIT_TRANSPORT_MIRROR
 
 // #define ENCODERS_PAD_A { GP27 }
 // #define ENCODERS_PAD_B { GP28 }
 
+// #define MATRIX_COL_PINS_LEFT { F5, F6, F7, B1, B3, B2, B6 }
+// #define MATRIX_COL_PINS_RIGHT { F5, F6, F7, B1, B3, B2, B6 }
+// #define MATRIX_COL_PINS_RIGHT { B6, B2, B3, B1, F7, F6, F5 }
