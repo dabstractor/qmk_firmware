2fc34f28f202a96fa6cdd291d94aba29738a10f9
Dustin Schultz <dustindschultz@gmail.com>
2025-03-03 13:50:10 -0500

    added powerscroll and toggle_vim tap dance code

--- keyboards/handwired/dactyl_manuform/5x7_1/keymaps/default/tap_dance.c
+++ keyboards/handwired/dactyl_manuform/5x7_1/keymaps/default/tap_dance.c
@@ -1,27 +1,39 @@
 #include QMK_KEYBOARD_H
+#include "./qmk-vim/src/vim.h"
 #include "leader.h"
 
 enum {
-    TD_LEAD_FN
+    TD_LEAD_FN,
+    TD_TG_VIM
 };
+
+void fn_layer_dance(tap_dance_state_t *state, callback_t fn) {
+    if(state->pressed) {
+        layer_on(_FN);
+    } else {
+        fn();
+    }
+}
+
+void fn_layer_reset(tap_dance_state_t *state, void *user_data) {
+    layer_off(_FN);
+}
 
 void qmk_leader(void) {
     leader_sequence_active() ? leader_end() : leader_start();
 }
 
 void leader_fn_each(tap_dance_state_t *state, void *user_data) {
-    if (state->pressed) {
-        layer_on(_FN);
-    } else {
-        qmk_leader();
-    }
+    fn_layer_dance(state, &qmk_leader);
 }
 
-void leader_fn_reset(tap_dance_state_t *state, void *user_data) {
-    layer_off(_FN);
+void vim_fn_each(tap_dance_state_t *state, void *user_data) {
+    fn_layer_dance(state, &toggle_vim_mode);
 }
+
 
 tap_dance_action_t tap_dance_actions[] = {
     // press and release for leader key, hold for fn layer
-    [TD_LEAD_FN] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, leader_fn_each, leader_fn_reset)
+    [TD_LEAD_FN] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, &leader_fn_each, &fn_layer_reset),
+    [TD_TG_VIM]  = ACTION_TAP_DANCE_FN_ADVANCED(NULL, &vim_fn_each, &fn_layer_reset)
 };
