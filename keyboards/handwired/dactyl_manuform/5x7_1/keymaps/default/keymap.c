115ce1c2a6c58f5e8193680cd9d273d38e41e2d9
Dustin Schultz <dustindschultz@gmail.com>
2025-03-03 13:17:06 -0500

    added copy/paste macros and moved to their own file

--- keyboards/handwired/dactyl_manuform/5x7_1/keymaps/default/keymap.c
+++ keyboards/handwired/dactyl_manuform/5x7_1/keymaps/default/keymap.c
@@ -6,6 +6,7 @@
 #include "./per_key_settings.c"
 #include "./custom_keycodes.c"
 #include "./layers.c"
+#include "./macros.c"
 #include "./tap_dance.c"
 
 // Each layer gets a name for readability, which is then used in the keymap matrix below.
@@ -250,114 +251,25 @@ bool process_custom_mod_tap_user(uint16_t keycode, keyrecord_t *record) {
     return true;
 }
 
-void double_space(uint16_t keycode, keyrecord_t *record) {
-    send_string("  ");
-}
-
-void toggle_vim(uint16_t keycode, keyrecord_t *record) {
-    toggle_vim_mode();
-}
-
-void click_this_spot(uint16_t keycode, keyrecord_t *record) {
-    tap_code(KC_MS_UP);
-    tap_code(KC_MS_DOWN);
-    tap_code(KC_MS_BTN1);
-}
-
-void workspace_left(uint16_t keycode, keyrecord_t *record) {
-    register_code(KC_RGUI);
-    register_code(KC_H);
-    unregister_code(KC_H);
-    unregister_code(KC_RGUI);
-}
-
-void workspace_right(uint16_t keycode, keyrecord_t *record) {
-    register_code(KC_RGUI);
-    register_code(KC_L);
-    unregister_code(KC_L);
-    unregister_code(KC_RGUI);
-}
-
-void to_default_layer(uint16_t keycode, keyrecord_t *record) {
-    layer_move(biton32(default_layer_state));
-}
-
-void toggle_default_layer(int layer) {
-    set_single_default_layer(default_layer_state == (1UL << layer) ? _QWERTY : layer);
-}
-
-void toggle_macos_default(uint16_t keycode, keyrecord_t *record) {
-    toggle_default_layer(_MAC);
-}
-
-void toggle_colemak_default(uint16_t keycode, keyrecord_t *record) {
-    toggle_default_layer(_COLEMAK);
-}
-
-#define POWERSCROLL_FACTOR 10
-
-void repeat_code(uint16_t keycode) {
-    for(int i = 0; i < POWERSCROLL_FACTOR; i++) {
-        register_code(keycode);
-        unregister_code(keycode);
-    }
-}
-
-void powerscroll_up(uint16_t keycode, keyrecord_t *record) {
-    repeat_code(KC_MS_WH_UP);
-}
-
-void powerscroll_down(uint16_t keycode, keyrecord_t *record) {
-    repeat_code(KC_MS_WH_DOWN);
-}
-
-void term_toggle(uint16_t keycode, keyrecord_t *record) {
-    // send GUI+Space
-    register_code(KC_RALT);
-    register_code(KC_SPC);
-    unregister_code(KC_RALT);
-    unregister_code(KC_SPC);
-}
-
-void tmux_leader(uint16_t keycode, keyrecord_t *record) {
-    // send Ctrl+Space
-    register_code(KC_RCTL);
-    register_code(KC_SPC);
-    unregister_code(KC_RCTL);
-    unregister_code(KC_SPC);
-}
-
-void activate_caps_word_toggle(uint16_t keycode, keyrecord_t *record) {
-    caps_word_toggle();
-}
-
-void toggle_mouse_layer(uint16_t keycode, keyrecord_t *record) {
-    layer_invert(_MOUSE);
-}
-
-void colon(uint16_t keycode, keyrecord_t *record) {
-    register_code(KC_LSFT);
-    register_code(KC_SCLN);
-    unregister_code(KC_LSFT);
-    unregister_code(KC_SCLN);
-}
-
 KeyCallback CUSTOM_KEYS[] = {
-    { DBL_SPACE, double_space },
-    { TOGGLE_VIM, toggle_vim },
-    { CLICK_THIS_SPOT, click_this_spot },
-    { WORKSPACE_LEFT, workspace_left },
-    { WORKSPACE_RIGHT, workspace_right },
-    { POWERSCROLL_UP, powerscroll_up },
-    { POWERSCROLL_DOWN, powerscroll_down },
-    { TO_DEFAULT_LAYER, to_default_layer },
-    { TOGGLE_MAC, toggle_macos_default },
-    { TOGGLE_COLEMAK, toggle_colemak_default },
-    { TERM_TOGGLE, term_toggle },
-    { TMUX_LEADER, tmux_leader },
-    { CAPS_WORD_TOGGLE, activate_caps_word_toggle },
-    { TOGGLE_MOUSE, toggle_mouse_layer },
-    { COLON, colon }
+    { DBL_SPACE, &double_space },
+    { TOGGLE_VIM, &toggle_vim },
+    { CLICK_THIS_SPOT, &click_this_spot },
+    { WORKSPACE_LEFT, &workspace_left },
+    { WORKSPACE_RIGHT, &workspace_right },
+    { POWERSCROLL_UP, &powerscroll_up },
+    { POWERSCROLL_DOWN, &powerscroll_down },
+    { TO_DEFAULT_LAYER, &to_default_layer },
+    { TOGGLE_MAC, &toggle_macos_default },
+    { TOGGLE_COLEMAK, &toggle_colemak_default },
+    { TERM_TOGGLE, &term_toggle },
+    { TMUX_LEADER, &tmux_leader },
+    { CAPS_WORD_TOGGLE, &activate_caps_word_toggle },
+    { TOGGLE_MOUSE, &toggle_mouse_layer },
+    { SELECT_ALL, &select_all },
+    { COPY, &copy },
+    { PASTE, &paste },
+    { COLON, &colon }
 };
 
 bool process_custom_key_user(uint16_t keycode, keyrecord_t *record) {
