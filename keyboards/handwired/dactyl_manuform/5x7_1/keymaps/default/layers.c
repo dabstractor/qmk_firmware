22f3aad94acbce95897dda9fab2740e5662a0ad8
Dustin Schultz <dustindschultz@gmail.com>
2025-03-03 13:17:06 -0500

    added copy/paste macros and moved to their own file

--- keyboards/handwired/dactyl_manuform/5x7_1/keymaps/default/layers.c
+++ keyboards/handwired/dactyl_manuform/5x7_1/keymaps/default/layers.c
@@ -1,13 +1,9 @@
-#define _QWERTY  0
-#define _COLEMAK 1
-#define _MAC     2
-#define _FN      3
-#define _NUMPAD  4
-#define _CMD     5
-#define _MOUSE   6
+#pragma once
 
-// Some basic macros
-#define TASK   LCTL(LSFT(KC_ESC))
-#define TAB_R  LCTL(KC_TAB)
-#define TAB_L  LCTL(LSFT(KC_TAB))
-#define TAB_RO LCTL(LSFT(KC_T))
+#define _QWERTY      0
+#define _COLEMAK     1
+#define _MAC         2
+#define _FN          3
+#define _NUMPAD      4
+#define _CMD         5
+#define _MOUSE       6
