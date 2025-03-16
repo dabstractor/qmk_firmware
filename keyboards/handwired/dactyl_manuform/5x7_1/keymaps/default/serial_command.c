d795781e1292b1d9ce9006eedfa7adcbbdb530eb
Dustin Schultz <dustindschultz@gmail.com>
2025-03-16 19:44:51 -0400

    added vim_lazy_insert to serial command code

--- keyboards/handwired/dactyl_manuform/5x7_1/keymaps/default/serial_command.c
+++ keyboards/handwired/dactyl_manuform/5x7_1/keymaps/default/serial_command.c
@@ -5,19 +5,28 @@
 #include "wait.h"
 #include "./layers.c"
 #include "./custom_keycodes.c"
+#include "./vim_lazy_insert.c"
 
-void vim_insert(void) {
-    enable_vim_mode();
-    insert_mode();
-}
+extern void vim_lazy_insert(void);
 
 DEFINE_SERIAL_COMMANDS({
     { "neovide", &disable_vim_mode },
-    { WT("google-chrome", "*claude - google*"), &vim_insert, &disable_vim_mode },
-    { WT("google-chrome", "*chatgpt - google*"), &vim_insert, &disable_vim_mode },
-    { WT("google-chrome", "deepseek* - google*"), &vim_insert, &disable_vim_mode },
+    { "alacritty", &disable_vim_mode },
+    { "*iterm*", &disable_vim_mode },
+    { "*claude*", &vim_lazy_insert, &disable_vim_mode },
+    { WT("*chrome*", "*claude*"), &vim_lazy_insert, &disable_vim_mode },
+    { WT("*chrome*", "*chatgpt*"), &vim_lazy_insert, &disable_vim_mode },
+    { WT("*chrome*", "*deepseek*"), &vim_lazy_insert, &disable_vim_mode }
 });
 
 DEFINE_SERIAL_LAYERS({
     { "*calculator", _NUMPAD },
+    { WT("*chrome*", "*jitsi*"), _JITSI },
+    { WT("alacritty", "terminal"), _TERMINAL },
+    { WT("alacritty", "alacritty"), _TERMINAL },
+    { "*iterm*", _TERMINAL },
+    { WT("*alacritty*", "*matterhorn*"), _MATTERHORN },
+    { "*clickup*", _CLICKUP },
+    { "*neovide*", _NEOVIM },
+    { "*chrome*", _CHROME }
 });
